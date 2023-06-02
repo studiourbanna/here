3cbb5ed8943bd2f36ba7a7147f75c6d3610c15b7
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2023-06-01 23:22:38 -0300

    Update Menu and Set Animation and Reponsive

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,35 +1,4 @@
-document.getElementById("myinput").onclick = function () {
-    var link = document.getElementById("linkinput").value;
-    var data = {
-        "domain": "do.encurtai.com",
-        "originalURL": link,
-        "allowDuplicates": false
-    };
-    fetch('https://api.short.cm/links/public', {
-        method: 'post',
-        headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
-            'authorization': 'FXV9LhBc44cjRRsLgyP6vg7DTUgrXvmY'
-        },
-        body: JSON.stringify(data)
-    }).then(function (response) {
-        return response.json();
-    })
-        .then(function (data) {
-            document.getElementById("message").innerHTML =
-                "<div class='notification is-link is-light'>"+
-                  "<div class='icon-text'>"+
-                    "<span class='icon has-text-info'><i class='fas fa-info-circle'></i></span>"+
-                    "<span>Seu novo link é: " +
-                    "<strong><a href='" + data.shortURL + "' target='_blank' class='is-white'>"
-                      + data.shortURL + "</a></strong>"+
-                    "</span>" +
-                  "</div>"+
-              "</div>"
-        })
-    document.getElementById("linkinput").value = '';
-}
+//MENU
 
 document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
@@ -63,3 +32,52 @@ $(document).ready(function () {
     $(".navbar-menu").toggleClass("is-active");
   });
 });
+
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});
+
+
+//Encurtador
+
+document.getElementById("myinput").onclick = function () {
+  var link = document.getElementById("linkinput").value;
+  var data = {
+    domain: "do.encurtai.com",
+    originalURL: link,
+    allowDuplicates: false
+  };
+  fetch("https://api.short.cm/links/public", {
+    method: "post",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+      authorization: "FXV9LhBc44cjRRsLgyP6vg7DTUgrXvmY"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.getElementById("message").innerHTML =
+        "<div class='notification is-link is-light'>" +
+        "<div class='icon-text'>" +
+        "<span class='icon has-text-info'><i class='fas fa-info-circle'></i></span>" +
+        "<span>Seu novo link é: " +
+        "<strong><a href='" +
+        data.shortURL +
+        "' target='_blank' class='is-white'>" +
+        data.shortURL +
+        "</a></strong>" +
+        "</span>" +
+        "</div>" +
+        "</div>";
+    });
+  document.getElementById("linkinput").value = "";
+};
