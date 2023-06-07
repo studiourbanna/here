5e27a4a1f3c5b33c55cb5c8c90c5a12b00e3b821
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2023-06-07 10:46:37 -0300

    Move Public Folder

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const pug = require('pug'); // And we couldn't render Pug without Pug.
 require('jstransformer-markdown-it'); // This needs to be installed for Pug to use it.
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('/'));
 
 app.get('/', function (req, res) {
-    res.send(pug.renderFile('public/index.pug')); 
+    res.send(pug.renderFile('/index.pug')); 
 });
 
 app.get('/home', function (req, res) {
-    res.send(pug.renderFile('public/index.pug')); 
+    res.send(pug.renderFile('/index.pug')); 
 });
 
 // Add more URLs and Pug files here...
 app.get('/404', function (req, res) {
-    res.send(pug.renderFile('public/error.pug')); 
+    res.send(pug.renderFile('/error.pug')); 
 });
 
 app.get('/error', function (req, res) {
-    res.send(pug.renderFile('public/error.pug')); 
+    res.send(pug.renderFile('/error.pug')); 
 });
 
 app.get('/about', function (req, res) {
-    res.send(pug.renderFile('public/about.pug')); 
+    res.send(pug.renderFile('/about.pug')); 
 });
 
 app.get('/terms', function (req, res) {
-    res.send(pug.renderFile('public/terms.pug')); 
+    res.send(pug.renderFile('/terms.pug')); 
 });
 
 app.listen(8080, function() {
