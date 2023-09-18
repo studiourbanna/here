334e47ebfdb63ee21fb1572ebd2b3918f7103dd3
Camila L. Oliveira <suporte@camilaloliveira.com.br>
2023-09-18 20:35:34 -0300

    Fix Conditionals

--- assets/js/shorter.js
+++ assets/js/shorter.js
@@ -1,52 +1,35 @@
-//Encurtador
+/*
+ * Encurtador de links
+ * Autor: Camila Oliveira
+ * Data: 18/09/2023
+ */
+
+const brandLinks = /ourbanna\.com|portalurbanna\.com\.br/;
+var domain = "";
 
 document.getElementById("myinput").onclick = function () {
-    var link = document.getElementById("linkinput").value;
-    if (link == "") {
-      document.getElementById("message").innerHTML =
-          "<div class='notification is-danger is-danger'>" +
-          "<div class='icon-text'>" +
-          "<span class='icon has-text-danger'><i class='fas fa-info-warning'></i></span>" +
-          "<span> Hm, parece que o link não existe, vamos tentar novamente?</span>" +
-          "</span>" +
-          "</div>" +
-          "</div>";
-    } else if (link == "ourbanna.com" || link == "portalurbanna.com.br/") {
-      var data = {
-        domain: "n.ourbanna.com",
-        originalURL: link,
-        allowDuplicates: false
-      };
-      fetch("https://api.short.cm/links/public", {
-        method: "post",
-        headers: {
-          accept: "application/json",
-          "Content-Type": "application/json",
-          authorization: "FXV9LhBc44cjRRsLgyP6vg7DTUgrXvmY"
-        },
-        body: JSON.stringify(data)
-      })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          document.getElementById("message").innerHTML =
-            "<div class='notification is-link is-light'>" +
-            "<div class='icon-text'>" +
-            "<span class='icon has-text-info'><i class='fas fa-info-circle'></i></span>" +
-            "<span>Seu novo link é: " +
-            "<strong><a href='" +
-            data.shortURL +
-            "' target='_blank' class='is-white'>" +
-            data.shortURL +
-            "</a></strong>" +
-            "</span>" +
-            "</div>" +
-            "</div>";
-        });
+  var link = document.getElementById("linkinput").value;
+  if (!link.trim() || link < "https://" || link < "http://") {
+    console.log("O link está vazio.");
+    document.getElementById("message").innerHTML =
+      "<div class='notification is-danger is-danger'>" +
+      "<div class='icon-text'>" +
+      "<span class='icon has-text-danger'><i class='fas fa-info-warning'></i></span>" +
+      "<span><strong>Hm, parece que o link não existe, vamos tentar novamente?</strong></span>" +
+      "</span>" +
+      "</div>" +
+      "</div>";
+  } else {
+    // Verifica se o link não contém os domínios permitidos
+    if (!brandLinks.test(link)) {
+      domain = "do.encurtai.com";
+      console.log("O link não contém os domínios permitidos.");
     } else {
-      var data = {
-      domain: "do.encurtai.com",
+      domain = "n.ourbanna.com";
+      console.log("O link contém um dos domínios permitidos.");
+    }
+    var data = {
+      domain: domain,
       originalURL: link,
       allowDuplicates: false
     };
@@ -78,4 +61,5 @@ document.getElementById("myinput").onclick = function () {
           "</div>";
       });
     document.getElementById("linkinput").value = "";
-  };
+  }
+};
